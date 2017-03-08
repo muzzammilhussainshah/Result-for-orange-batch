e38550237852330826832fae363c0c5211266a70
muzzammilhussainshah <muzammilhussain660@gmail.com>
2017-03-08 13:37:37 -0800

    result for orange section

---
+++ public/app.js
@@ -0,0 +1,73 @@
+var RollNumber = prompt("TYPE YOUR ROLL NUMBER (Top scorers in orange batch");
+var Html = "HTML";
+var Css = 'Css';
+var Css2 = 'Css2';
+ if (RollNumber == 5215) {
+  alert(Html+' '+ "72"+' '+Css+' '+"60"+' '+Css2+' '+"56"+' '+"(63%)");}
+ else if (RollNumber == 4799) {
+  alert(Html+' '+ "48"+' '+Css+' '+"68.57"+' '+Css2+' '+"68"+' '+"(62%)");}  
+else if (RollNumber ==5047) {
+  alert(Html+' '+ "78"+' '+Css+' '+"54.29"+' '+Css2+' '+"52"+' '+"(61%)");}
+else if (RollNumber ==5216 ) {
+  alert(Html+' '+ "74"+' '+Css+' '+"54.29"+' '+Css2+' '+"56"+' '+"(61%)");}
+  else if (RollNumber ==4679) {
+  alert(Html+' '+ "72"+' '+Css+' '+"40"+' '+Css2+' '+"72"+' '+"(61%)");}
+    else if (RollNumber ==6014) {
+  alert(Html+' '+ "66"+' '+Css+' '+"57.14"+' '+Css2+' '+"60"+' '+"(61%)");}
+    else if (RollNumber ==6178) {
+  alert(Html+' '+ "78"+' '+Css+' '+"57.14"+' '+Css2+' '+"48"+' '+"(61%)");}
+    else if (RollNumber ==5001 ) {
+  alert(Html+' '+ "72"+' '+Css+' '+"62.86"+' '+Css2+' '+"48"+' '+"(61%)");}
+    else if (RollNumber ==6728 ) {
+  alert(Html+' '+ "74"+' '+Css+' '+"68.57"+' '+Css2+' '+"40"+' '+"(61%)");}
+      else if (RollNumber ==3278) {
+  alert(Html+' '+ "66"+' '+Css+' '+"60"+' '+Css2+' '+"56"+' '+"(61%)");}
+      else if (RollNumber ==5046 ) {
+  alert(Html+' '+ "68"+' '+Css+' '+"57.14"+' '+Css2+' '+"56"+' '+"(60%)");}
+      else if (RollNumber ==6435 ) {
+  alert(Html+' '+ "72"+' '+Css+' '+"57.14"+' '+Css2+' '+"52"+' '+"(60%)");}
+      else if (RollNumber ==7174) {
+  alert(Html+' '+ "92"+' '+Css+' '+"0"+' '+Css2+' '+"88"+' '+"(60%)");}
+      else if (RollNumber == 3914) {
+  alert(Html+' '+ "74"+' '+Css+' '+"57.14"+' '+Css2+' '+"48"+' '+"(60%)");}
+      else if (RollNumber ==5347 ) {
+  alert(Html+' '+ "80"+' '+Css+' '+"62.86"+' '+Css2+' '+"36"+' '+"(60%)");}
+      else if (RollNumber ==5425	 ) {
+  alert(Html+' '+ "52"+' '+Css+' '+"62.86"+' '+Css2+' '+"64"+' '+"(60%)");}
+        else if (RollNumber ==5742 ) {
+  alert(Html+' '+ "76"+' '+Css+' '+"62.86"+' '+Css2+' '+"40"+' '+"(60%)");}
+        else if (RollNumber ==3513 ) {
+  alert(Html+' '+ "66"+' '+Css+' '+"60"+' '+Css2+' '+"52"+' '+"(59%)");}
+        else if (RollNumber ==3833 ) {
+  alert(Html+' '+ "70"+' '+Css+' '+"60"+' '+Css2+' '+"48"+' '+"(59%)");}
+        else if (RollNumber ==4747 ) {
+  alert(Html+' '+ "78"+' '+Css+' '+"60"+' '+Css2+' '+"40"+' '+"(%)");}
+        else if (RollNumber ==5162 ) {
+  alert(Html+' '+ "74"+' '+Css+' '+"40"+' '+Css2+' '+"64"+' '+"(59%)");}
+          else if (RollNumber ==5446 ) {
+  alert(Html+' '+ "62"+' '+Css+' '+"60"+' '+Css2+' '+"56"+' '+"(59%)");}
+          else if (RollNumber ==5847 ) {
+  alert(Html+' '+ "66"+' '+Css+' '+"51.43"+' '+Css2+' '+"60"+' '+"(59%)");}
+          else if (RollNumber ==6628 ) {
+  alert(Html+' '+ "62"+' '+Css+' '+"71.43"+' '+Css2+' '+"44"+' '+"(59%)");}
+          else if (RollNumber == 6906) {
+  alert(Html+' '+ "70"+' '+Css+' '+"51.43"+' '+Css2+' '+"56"+' '+"(59%)");}
+          else if (RollNumber ==3355 ) {
+  alert(Html+' '+ "80"+' '+Css+' '+"37.14"+' '+Css2+' '+"60"+' '+"(59%)");}
+          else if (RollNumber ==3393 ) {
+  alert(Html+' '+ "80"+' '+Css+' '+"37.14"+' '+Css2+' '+"60"+' '+"(59%)");}
+          else if (RollNumber ==4449 ) {
+  alert(Html+' '+ "50"+' '+Css+' '+"62.86"+' '+Css2+' '+"64"+' '+"(59%)");}
+          else if (RollNumber ==4585 ) {
+  alert(Html+' '+ "56"+' '+Css+' '+"60"+' '+Css2+' '+"60"+' '+"(59%)");}
+  else if (RollNumber ==5217 ) {
+  alert('YOU ARE A BOKA DEVELOPER dubara apna roll number dala to system se nikal k marunga'+' '+Html+' '+ "64"+' '+Css+' '+"42"+' '+Css2+' '+"48"+' '+"(52%)");}
+else if (RollNumber =='') {
+  alert('empty content');}
+          else if (RollNumber <=9999999999999999999999999999999999999999999 ) {
+  alert('Sorry its not a part of top scorers in orange batch');}
+   else if (RollNumber >=0.9999999999999999999999999999999999999999999 ) {
+  alert('Sorry its not a part of top scorers in orange batch');}
+  
+          else{
+  alert("Sorry something went wrong");}
